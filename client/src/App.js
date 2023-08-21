f161839daa5d0e3d4328ffbc96a1ba71bb5cbbe6
newSung <gho0612@gmail.com>
2023-08-21 11:51:31 +0900

    server test

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,32 @@ import Auth from './hoc/auth'
 
 function App() {
 
-  const AuthLandingPage = Auth(LandingPage, false);
-  const AuthLoginPage = Auth(LoginPage, false);
-  const AuthRegisterPage = Auth(RegisterPage, false);
-  const AuthLandingPage2 = Auth(LandingPage2, true);
-  const AuthNewDiaryPage = Auth(NewDiaryPage, true);
-  const AuthShowDiaryPage = Auth(ShowDiaryPage, true);
-  const AuthMyPage = Auth(MyPage, true);
+  // const AuthLandingPage = Auth(LandingPage, false);
+  // const AuthLoginPage = Auth(LoginPage, false);
+  // const AuthRegisterPage = Auth(RegisterPage, false);
+  // const AuthLandingPage2 = Auth(LandingPage2, true);
+  // const AuthNewDiaryPage = Auth(NewDiaryPage, true);
+  // const AuthShowDiaryPage = Auth(ShowDiaryPage, true);
+  // const AuthMyPage = Auth(MyPage, true);
 
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<AuthLandingPage />} />
+        {/* <Route exact path="/" element={<AuthLandingPage />} />
         <Route exact path="/after" element={<AuthLandingPage2 />} />
         <Route exact path="/login" element={<AuthLoginPage />} />
         <Route exact path="/register" element={<AuthRegisterPage />} />
         <Route exact path="/newdiary" element={<AuthNewDiaryPage />} />
         <Route exact path="/showdiary" element={<AuthShowDiaryPage />} />
-        <Route exact path="/mypage" element={<AuthMyPage />} />
-
+        <Route exact path="/mypage" element={<AuthMyPage />} /> */}
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/after" element={<LandingPage2 />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/newdiary" element={<NewDiaryPage />} />
+        <Route exact path="/showdiary" element={<ShowDiaryPage />} />
+        <Route exact path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
