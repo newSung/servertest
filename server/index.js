e2b494a8e6c210987a5526a6dac64780fed8a665
newSung <gho0612@gmail.com>
2023-08-21 18:34:19 +0900

    z

--- server/index.js
+++ server/index.js
@@ -14,12 +14,9 @@ const cookieParser = require('cookie-parser')
 
 const { auth } = require("./middleware/auth")
 
-let corsOptions = {
-    origin: '*',      // 출처 허용 옵션
-    credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
-}
-
-app.use(cors(corsOptions))
+app.use(cors({
+    origin: 'https://gregarious-meerkat-c611cb.netlify.app',
+}));
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
