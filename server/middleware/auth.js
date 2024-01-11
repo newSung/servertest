25d82872423286fbd94a4fee8a005fb53f743eac
newSung <gho0612@gmail.com>
2024-01-11 22:04:31 +0900

    test

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,6 +8,7 @@ let auth = (req, res, next) => {
 
     //토큰을 복호화 한후 유저를 찾는다
     User.findByToken(token, (err, user) => {
+        console.log(token)
         if (err) throw err;
         if (!user) return res.json({ isAuth: false, error: true })
 
