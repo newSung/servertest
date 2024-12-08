7eac830a3536adcd4b3a460ac54c87a45177b93d
newSung <108291773+newSung@users.noreply.github.com>
2024-12-08 20:08:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -45,13 +45,38 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-const mongoose = require('mongoose')
-//mongoose.set('strictQuery', true);
-// mongoose.connect(config.mongoURI, {
-mongoose.connect(process.env.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('db connected'))
-    .catch(err => console.log(err))
+// const mongoose = require('mongoose')
+// //mongoose.set('strictQuery', true);
+// // mongoose.connect(config.mongoURI, {
+// mongoose.connect(process.env.mongoURI, {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// }).then(() => console.log('db connected'))
+//     .catch(err => console.log(err))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://chat:D0101oCMiUb9m419@cluster0.aj12u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => { res.send('hellossssssssssssssss world') })
